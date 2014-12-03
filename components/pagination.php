f6bb7f64ceeb6489b09a42cf09a6560f7c3ef0bb
Anthony Plopper <anthony@cloudwebteam.com>
2014-12-03 16:11:14 -0600

    Added option to pass in  to pagination

--- components/pagination.php
+++ components/pagination.php
@@ -1,5 +1,8 @@
 <?php
-global $wp_query;
+if ( ! isset( $query ) ){
+	global $wp_query;
+	$query = $wp_query;
+}
 
 $big = 999999999; // need an unlikely integer
 
@@ -7,7 +10,7 @@ $pagination = paginate_links( array(
 	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 	'format' => '?paged=%#%',
 	'current' => max( 1, get_query_var('paged') ),
-	'total' => $wp_query->max_num_pages
+	'total' => $query->max_num_pages
 ) );
 if ( $pagination ){ ?>
 <div class="pagination">
