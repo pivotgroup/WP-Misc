e8c89500a63f76b8382fe40166e670d535d9632a
Anthony Plopper <anthony@cloudwebteam.com>
2014-12-03 16:15:37 -0600

    Refactored toggling to take arguments.

--- js/toggling.js
+++ js/toggling.js
@@ -1,38 +1,42 @@
-var toggling = ( function( ){
-	var init = function( $button, $item, args ){
-		args = args || {};
-		var openSpeed = args.openSpeed || 200;
-		var closeSpeed = args.closeSpeed || 200;
-		var _open = false;
-		var open = function(){
-			_open = true;
-			$item.slideDown( openSpeed );
-			setTimeout( function(){
-				$window.on( 'mousedown.mobile-menu touchstart.mobile-menu', function(e){
-					if ( $(e.target).closest( $item.add( $button ) ).size() === 0 ){
-						close();
-					}
-				});
-			}, 100 );
-		}
-		var close = function(){
-			$item.slideUp( closeSpeed, function(){
-				_open = false;
-			});
-			$window.unbind( 'mousedown.mobile-menu touchstart.mobile-menu' );
-		}
-		function toggleItem(e) {
-			e.preventDefault();
-			e.stopPropagation();
-			if( _open ) {
-				close();
-			} else {
-				open();
+	var toggling = ( function( ){
+		var init = function( $button, $item, args ){
+			args = $.extend( {}, {
+				openSpeed: 200,
+				closeSpeed: 200,
+				activeClass: 'active'
+			}, args);
+			var _open = false;
+			var open = function(){
+				_open = true;
+				$item.slideDown( args.openSpeed );
+				$button.addClass( args.activeClass );
+				setTimeout( function(){
+					$window.on( 'mousedown.mobile-menu touchstart.mobile-menu', function(e){
+						if ( $(e.target).closest( $item.add( $button ) ).size() === 0 ){
+							close();
+						}
+					});
+				}, 100 );
 			}
-		}
-		$button.on( 'click touchstart', toggleItem );
-	};
-	return {
-		init: init
-	};
-}() );
+			var close = function(){
+				$button.removeClass( args.activeClass );
+				$item.slideUp( args.closeSpeed, function(){
+					_open = false;
+				});
+				$window.unbind( 'mousedown.mobile-menu touchstart.mobile-menu' );
+			}
+			function toggleItem(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				if( _open ) {
+					close();
+				} else {
+					open();
+				}
+			}
+			$button.on( 'click touchstart', toggleItem );
+		};
+		return {
+			init: init
+		};
+	}() );
