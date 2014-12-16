8598afb7d9d5d0dcd7f658ef98f7b769db531c0f
Anthony Plopper <anthony@cloudwebteam.com>
2014-12-16 11:57:41 -0600

    Fixed bug with wp_get_attachment_image()

--- components/recent_posts.php
+++ components/recent_posts.php
@@ -3,18 +3,13 @@ $query_args = array(
 	'post_type' => 'post',
 	'posts_per_page' => $number
 );
-// array of post_ids
-if ( isset( $posts ) ){
-	$query_args[ 'post__in' ] = $posts;
-	$query_args[ 'orderby' ] = 'post__in';
-}
 $query = new WP_Query( $query_args );
 if ( !$query->have_posts() ) return;
 global $post, $Theme; ?>
 <div class="post-list latest-posts">
 <?php while( $query->have_posts() ): $query->the_post();
 	$excerpt = $post->post_excerpt ? $post->post_excerpt : $post->post_content;
-	$featured_image = wp_get_attachment_image( $post ); ?>
+	$featured_image = wp_get_attachment_image( get_the_post_thumbnail( $post ) ); ?>
 	<div <?php post_class(); ?>>
 		<div class="date"><?php the_time( 'F j, Y' ); ?></div>
 		<?php if ( $featured_image ){ ?>
