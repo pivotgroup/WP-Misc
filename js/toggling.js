e22c86cf5267760bc31b5c8a969fa7db8c7dd790
Anthony Plopper <anthony@cloudwebteam.com>
2014-12-16 11:58:16 -0600

    Updated toggling to have callbacks and switchable effect.

--- js/toggling.js
+++ js/toggling.js
@@ -1,42 +1,60 @@
-	var toggling = ( function( ){
-		var init = function( $button, $item, args ){
-			args = $.extend( {}, {
-				openSpeed: 200,
-				closeSpeed: 200,
-				activeClass: 'active'
-			}, args);
-			var _open = false;
-			var open = function(){
-				_open = true;
-				$item.slideDown( args.openSpeed );
-				$button.addClass( args.activeClass );
-				setTimeout( function(){
-					$window.on( 'mousedown.mobile-menu touchstart.mobile-menu', function(e){
-						if ( $(e.target).closest( $item.add( $button ) ).size() === 0 ){
-							close();
-						}
-					});
-				}, 100 );
-			}
-			var close = function(){
-				$button.removeClass( args.activeClass );
-				$item.slideUp( args.closeSpeed, function(){
-					_open = false;
+var toggling = ( function( ){
+	var init = function( $button, $item, args ){
+		args = $.extend( {}, {
+			openSpeed: 200,
+			closeSpeed: 200,
+			activeClass: 'active',
+			effect: 'slide',
+			onOpen: function(){},
+			onAfterOpen: function(){},
+			onClose: function(){},
+			onAfterClose: function(){}
+		}, args);
+		var _open = false;
+		if ( args.effect === 'fade' ){
+			var hideFunction = 'fadeOut';
+			var showFunction = 'fadeIn';
+		} else {
+			var hideFunction = 'slideUp';
+			var showFunction = 'slideDown';
+		}
+
+		var open = function(){
+			_open = true;
+			$item[ showFunction ]( args.openSpeed, function(){
+				args.onAfterOpen.apply( $item[0] );
+			});
+			$button.addClass( args.activeClass );
+			args.onOpen.apply( $item[0] );
+			setTimeout( function(){
+				$window.on( 'mousedown.mobile-menu touchstart.mobile-menu', function(e){
+					if ( $(e.target).closest( $item.add( $button ) ).size() === 0 ){
+						close();
+					}
 				});
-				$window.unbind( 'mousedown.mobile-menu touchstart.mobile-menu' );
+			}, 100 );
+		}
+		var close = function(){
+			$button.removeClass( args.activeClass );
+			args.onClose.apply( $item[0] );
+			$item[ hideFunction ]( args.closeSpeed, function(){
+				_open = false;
+				args.onAfterClose.apply( $item[0] );
+			});
+			$window.unbind( 'mousedown.mobile-menu touchstart.mobile-menu' );
+		}
+		function toggleItem(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			if( _open ) {
+				close();
+			} else {
+				open();
 			}
-			function toggleItem(e) {
-				e.preventDefault();
-				e.stopPropagation();
-				if( _open ) {
-					close();
-				} else {
-					open();
-				}
-			}
-			$button.on( 'click touchstart', toggleItem );
-		};
-		return {
-			init: init
-		};
-	}() );
+		}
+		$button.on( 'click touchstart', toggleItem );
+	};
+	return {
+		init: init
+	};
+}() );
