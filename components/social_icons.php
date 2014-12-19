a7527671ad49832eea8eb4b7722001d7f60cb913
Anthony Plopper <anthony@cloudwebteam.com>
2014-12-19 12:41:38 -0600

    Added class generated from title att

--- components/social_icons.php
+++ components/social_icons.php
@@ -1,10 +1,10 @@
 <?php
 if ( !isset( $social_icons ) ){
-  $social_icons = get_theme_options( 'cloud_options', 'global', 'social_icons' );
+  $social_icons = get_theme_options( 'theme_options', 'global', 'social_icons' );
 }
 if ( ! $social_icons ) return; ?>
 <ul class="social-icons">
 	<?php foreach( $social_icons as $icon ){ ?>
-	<li><?php echo sprintf( $icon['link']['sprintf'], $icon['icon'] ); ?></li>
+	<li class="<?= sanitize_title( $icon['link']['title'] ); ?>"><?php echo sprintf( $icon['link']['sprintf'], $icon['icon'], '' ); ?></li>
 	<?php } ?>
 </ul>
