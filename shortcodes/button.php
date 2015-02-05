c627b6f59fd96c4c4c7b9f1e1f8b858d7777343d
Anthony Plopper <anthony@anthonyplopper.com>
2015-02-05 16:20:49 -0600

    Create button.php

---
+++ shortcodes/button.php
@@ -0,0 +1,19 @@
+<?php $classes = array( 'button' );
+if ( !empty( $has_arrow ) ) $classes[] = 'has-arrow';
+
+echo sprintfLink( $link, $text, implode( ' ', $classes ) ); ?>
+
+/* ==== TO ADD ============================================= */
+	'button' => array(
+		'fields' => array(
+			'text' => array(),
+			'link' => array(
+				'type' => 'link'
+			),
+			'has_little_arrow' => array(
+				'type' => 'checkbox',
+				'title' => '',
+				'checkbox_label' => 'Has Little Arrow'
+			)
+		)
+	)
