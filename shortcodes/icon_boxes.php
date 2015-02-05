1c36d507c93ddff34b081a66fb7b0b0cb1fd4817
Anthony Plopper <anthony@anthonyplopper.com>
2015-02-05 17:34:33 -0600

    Update icon_boxes.php

--- shortcodes/icon_boxes.php
+++ shortcodes/icon_boxes.php
@@ -2,15 +2,17 @@
 	return;
 }
 ?>
-<div class="icon-boxes number-boxes-<?= sizeof( $boxes ); ?>">
+<div class="icon-boxes number-boxes-<?= sizeof( $boxes ); ?> cf">
 	<?php foreach( $boxes as $box ){
 		$target = $box['link']['target'] ? 'target="_blank"' : '';
 		$title = $box['link']['title'] ? 'title="'.$box['link']['title'].'"' : ''; ?>
-	<div class="box">
+	<div class="icon-box">
 		<a href="<?= $box['link']['url']; ?>" <?= $target; ?> <?= $title; ?>>
 			<span class="icon">
-				<?php if ( !empty( $box['icon'] ) ){ ?>
-				<i class="fa fa-<?= $box['icon']; ?>"></i>
+				<?php if ( !empty( $box['icon_type']) && $box['icon_type'] === 'svg' ){ ?>
+					<span class="image-container"><?= getMediaFromID( $box['svg'], 'image' ); ?></span>
+				<?php } else if ( !empty( $box['icon'] ) ){ ?>
+					<i class="fa fa-<?= $box['icon']; ?>"></i>
 				<?php } ?>
 			</span>
 			<span class="title"><?= $box['title']; ?></span>
@@ -20,7 +22,6 @@
 </div>
 
 // to add
-$Forms->add_shortcodes( array(
 	'icon_boxes' => array(
 		'fields' => array(
 			'boxes' => array(
@@ -28,8 +29,20 @@ $Forms->add_shortcodes( array(
 				'cloneable' => true,
 				'subfields' => array(
 					'title' => array(),
+					'icon_type' => array(
+						'type' => 'radio',
+						'default' => 'fa',
+						'options' => array( 'fa' => 'FontAwesome', 'svg' => 'SVG upload' )
+					),
 					'icon' => array(
-						'description' => 'FontAwesome Icon. eg. camera'
+						'description' => 'FontAwesome Icon. eg. camera',
+						'toggle' => array( 'icon_type' => '!svg' )
+					),
+					'svg' => array(
+						'type' => 'media',
+						'mime' => 'image',
+						'description' => 'SVG image here, please',
+						'toggle' => array( 'icon_type' => 'svg' )
 					),
 					'link' => array(
 						'type' => 'link'
@@ -37,5 +50,4 @@ $Forms->add_shortcodes( array(
 				)
 			)
 		)
-	)
-));
+	),
