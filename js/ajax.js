7b1fbbad999892af8e901bb802a4c60062821e1e
Anthony Plopper <anthony@anthonyplopper.com>
2015-04-02 16:17:54 -0500

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -20,3 +20,71 @@ var ajax = ( function(){
 		exampleAction: exampleAction
 	};
 }() );
+
+// WP with simple cache
+var ajax = ( function(){
+	function wpAjaxCall( data, cb ){
+		cb = cb || function(){};
+		return $.ajax({
+			url: wpVars.ajax_url, // wp ajax url
+			method: 'post',
+			data: data,
+			dataType: 'json',
+			success: cb
+		});
+	}
+	var _cache = {};
+	function cacheHasher( args ){
+		var hash = [];
+		for (i in args ){
+			if ( args.hasOwnProperty(i)){
+				if ( typeof( args[i] ) === 'number' || typeof( args[i] ) === 'string' ){
+					hash.push( i+':'+args[i] );
+				} else if ( typeof( args[i] ) !== 'function' ){
+					hash.push( i+':'+JSON.stringify( args[i] ));
+				}
+			}
+		}
+		return hash.sort().join(',');
+	}
+	function cacheResult( args, result ){
+		var fnct = args.callee.name;
+		if ( ! _cache.hasOwnProperty( fnct ) ){
+			_cache[ fnct ] = {};
+		}
+		var hash = cacheHasher( args );
+		_cache[ fnct ][ hash ] = result;
+		return hash;
+	}
+	function getCachedResult( args ){
+		var fnct = args.callee.name;
+		if ( typeof( _cache[ fnct ] ) !== 'undefined' ){
+			var hash = cacheHasher( args );
+			if ( typeof( _cache[ fnct ][ hash ] ) !== 'undefined' ){
+				return { result: _cache[ fnct ][ hash ] };
+			}
+		}
+		return false;
+	}
+	function getPostContent(id, cb){
+		if ( ! id ){
+			cb( false );
+			return;
+		}
+		var cached = getCachedResult( arguments );
+		if ( cached ){
+			cb( cached.result );
+			return;
+		}
+		var argmnts = arguments;
+		return wpAjaxCall({
+			action: 'tecore_get_post_content',
+			id: id
+		}, function( r ){
+			cb( r );
+		});
+	}
+	return {
+		getPostContent: getPostContent
+	};
+}() );
