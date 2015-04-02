a85ba57b34cc764853d7deb18486fa410a25d698
Anthony Plopper <anthony@anthonyplopper.com>
2015-04-02 14:55:01 -0500

    Update wp-enqueue.php magnific paths

--- wp-enqueue.php
+++ wp-enqueue.php
@@ -9,6 +9,6 @@ public function enqueue_touchcarousel(){
 	wp_enqueue_script( 'touchcarousel', $this->getDir( '/_inc/touchcarousel/jquery.touchcarousel-1.2.min.js' ), array( 'jquery' ), false, true );
 }
 protected function enqueue_magnific(){
-	wp_enqueue_script( 'magnific', $this->getDir( '_inc/magnific/jquery.magnific-popup.min.js'), array( 'jquery' ), false, true );
-	wp_enqueue_style( 'magnific', $this->getDir( '_inc/magnific/magnific-popup.css') );
+	wp_enqueue_script( 'magnific', $this->getDir( '_inc/js/magnific-popup/dist/jquery.magnific-popup.min.js'), array( 'jquery' ), false, true );
+	wp_enqueue_style( 'magnific', $this->getDir( '_inc/js/magnific-popup/dist/magnific-popup.css') );
 }
